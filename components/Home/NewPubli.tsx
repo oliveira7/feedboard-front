ebc515c4b0485224d2d3ea0f9f912940a485f7d0
yago-castro-gomes <yago.fanstour@gmail.com>
2024-11-18 23:28:43 -0300

    ajuste de build

--- components/Home/NewPubli.tsx
+++ components/Home/NewPubli.tsx
@@ -49,6 +49,7 @@ export default function NewPubli() {
 
     try {
       const response = await newPost(formData);
+      console.log(response);
       setAtualizarFeed(true);
       setLoading(true);
       setPostText('');
